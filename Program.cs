9abddd9e25b540a8db0383aba5178b8707102969
tinaheidinger <tinaheidinger@github.com>
2022-08-25 23:07:50 +0200

    library returns random jack sparrow quote

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace QuoteGenerator;
+
+public static class SparrowQuoteGenerator
+{
+    public static String GetRandomSparrowQuote()
+    {
+        String[] quotes = {
+        "The problem is not the problem. The problem is your attitude about the problem.",
+        "If you were waiting for the opportune moment, that was it.",
+        "This is the day you will always remember as the day you almost caught Captain Jack Sparrow.",
+        "Why fight when you can negotiate?",
+        "I'm dishonest, and a dishonest man you can always trust to be dishonest. Honestly."
+        };
+
+        var random = new Random();
+        var quote = quotes[random.Next(0, quotes.Length)];
+        return(quote);
+    }
+}
